acd920e15d857aba9ca72f00e38820ee6ed3b692
nuoxian <56551535+nuoxianCN@users.noreply.github.com>
2021-12-26 14:10:56 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    $host = 'localhost'; //数据库地址
+	$username = 'root'; //数据库登录账号
+	$password = 'root'; //数据库登录密码
+	$dbname = 'root'; //你操作数据库的名字
+    $SQLtable = 'result'; //选择查询的数据库表名
+	$port = 3306; //数据库地址端口，一般是3306
+
+	$return = NULL; //是否查询到学生，此选项一般不要动。默认即可
+?>
