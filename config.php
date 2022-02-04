bf13872795a05435e43995b0bff5c3105e446b0b
nuoxian <56551535+nuoxianCN@users.noreply.github.com>
2022-02-04 13:06:08 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -3,8 +3,19 @@
 	$username = 'root'; //数据库登录账号
 	$password = 'root'; //数据库登录密码
 	$dbname = 'root'; //你操作数据库的名字
-    $SQLtable = 'result'; //选择查询的数据库表名
+    $SQLtable = 'result'; //选择查询的数据库表名，一般不动
 	$port = 3306; //数据库地址端口，一般是3306
 
-	$return = NULL; //是否查询到学生，此选项一般不要动。默认即可
+	// 查询后的文本，可自定义
+	$Custom_text1 = "学号"; //建议保持默认
+	$Custom_text2 = "姓名"; //建议保持默认
+	$Custom_text3 = "语文";
+	$Custom_text4 = "数学";
+	$Custom_text5 = "英语";
+	$Custom_text6 = "政治";
+	$Custom_text7 = "历史";
+	$Custom_text8 = "地理";
+	$Custom_text9 = "物理";
+	$Custom_text10 = "化学";
+	$Custom_text11 = "生物";
 ?>
