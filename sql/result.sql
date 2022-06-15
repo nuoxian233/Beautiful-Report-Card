0ba8723a514787dbd64899a2a91c1668dae241d2
nuoxian <56551535+nuoxianCN@users.noreply.github.com>
2022-06-15 20:31:40 +0800

    Add files via upload

--- sql/result.sql
+++ sql/result.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2022-02-04 13:01:15
+-- Generation Time: 2022-06-15 20:31:17
 -- 服务器版本： 5.6.50-log
 -- PHP Version: 5.6.40
 
@@ -27,9 +27,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `result` (
-  `stuid` bigint(20) NOT NULL COMMENT '考生号/准考证号',
-  `id` bigint(20) NOT NULL COMMENT '身份证号后四位',
+  `number` bigint(20) NOT NULL COMMENT '考生号',
+  `id` bigint(20) NOT NULL COMMENT '身份证号后六位',
   `name` varchar(14) NOT NULL COMMENT '姓名',
+  `remarks` text NOT NULL COMMENT '老师批语或备注',
   `custom_text1` varchar(14) NOT NULL COMMENT '对应配置文件custom_text3',
   `custom_text2` varchar(14) NOT NULL COMMENT '对应配置文件custom_text4',
   `custom_text3` varchar(14) NOT NULL COMMENT '对应配置文件custom_text5',
@@ -45,9 +46,9 @@ CREATE TABLE IF NOT EXISTS `result` (
 -- 转存表中的数据 `result`
 --
 
-INSERT INTO `result` (`stuid`, `id`, `name`, `custom_text1`, `custom_text2`, `custom_text3`, `custom_text4`, `custom_text5`, `custom_text6`, `custom_text7`, `custom_text8`, `custom_text9`) VALUES
-(202101, 2333, '小明', '100', '100', '100', '100', '100', '100', '100', '100', '100'),
-(202102, 2333, '小红', '100', '100', '0', '0', '100', '100', '0', '0', '0');
+INSERT INTO `result` (`number`, `id`, `name`, `remarks`, `custom_text1`, `custom_text2`, `custom_text3`, `custom_text4`, `custom_text5`, `custom_text6`, `custom_text7`, `custom_text8`, `custom_text9`) VALUES
+(202101, 233333, '小明', '你有很多优点，不必在意其他，做好自己，保持谦虚的做人风格', '100', '100', '100', '100', '100', '100', '100', '100', '100'),
+(202102, 233333, '小红', '在生活中得到经验，会对你有很大的帮助', '100', '100', '0', '0', '100', '100', '0', '0', '0');
 
 --
 -- Indexes for dumped tables
@@ -57,7 +58,7 @@ INSERT INTO `result` (`stuid`, `id`, `name`, `custom_text1`, `custom_text2`, `cu
 -- Indexes for table `result`
 --
 ALTER TABLE `result`
-  ADD PRIMARY KEY (`stuid`);
+  ADD PRIMARY KEY (`number`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
