bb04f38f4d3625dce14beeb8fb87637783ee5c20
nuoxian <56551535+nuoxianCN@users.noreply.github.com>
2022-06-15 20:41:27 +0800

    增加多样式主题

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
-    $host = 'localhost'; //数据库地址
-	$username = 'root'; //数据库登录账号
-	$password = 'root'; //数据库登录密码
-	$dbname = 'root'; //你操作数据库的名字
-    $SQLtable = 'result'; //选择查询的数据库表名，一般不动
-	$port = 3306; //数据库地址端口，一般是3306
+    $host = 'localhost'; // 
+	$username = 'root'; // 数据库登录账号
+	$password = 'root'; // 数据库登录密码
+	$dbname = 'root'; // 你操作数据库的名字
+    $SQLtable = 'result'; // 选择查询的数据库表名，一般不动
+	$port = 3306; // 数据库地址端口，一般是3306
 
+	$themes = "default"; // 引用的主题目录，在themes可以看到，默认default
+	
 	// 查询后的文本，可自定义
-	$Custom_text1 = "学号"; //建议保持默认
-	$Custom_text2 = "姓名"; //建议保持默认
+	$Custom_text1 = "学号"; // 建议保持默认
+	$Custom_text2 = "姓名"; // 建议保持默认
 	$Custom_text3 = "语文";
 	$Custom_text4 = "数学";
 	$Custom_text5 = "英语";
