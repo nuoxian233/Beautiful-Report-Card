b8e6ba5c264d20b954aeb1a3aad6514baac0d6c4
nuoxian <56551535+nuoxianCN@users.noreply.github.com>
2022-09-01 17:57:03 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -2,11 +2,16 @@
     $host = 'localhost'; // 
 	$username = 'root'; // 数据库登录账号
 	$password = 'root'; // 数据库登录密码
-	$dbname = 'root'; // 你操作数据库的名字
-    $SQLtable = 'result'; // 选择查询的数据库表名，一般不动
+	$dbname = 'root'; // 数据库的名字
+    $SQLtable = 'result'; // 选择查询的数据库表，一般不动
+	$SQLtable1 = 'result1'; // 选择查询的第二个数据库表
+	$SQLtable2 = 'result2'; // 选择查询的第三个数据库表
+	$SQLtable3 = 'result3'; // 选择查询的第四个数据库表
 	$port = 3306; // 数据库地址端口，一般是3306
 
 	$themes = "default"; // 引用的主题目录，在themes可以看到，默认default，还有一个concise主题
+
+	$admin_password = "123456"; // 后台登录的密码
 	
 	// 查询后的文本，可自定义
 	$Custom_text1 = "学号"; // 建议保持默认
