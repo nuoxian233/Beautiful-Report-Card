558e4216e2f48f9f6060237cb4e2aca886fe3302
nuoxianCN <nuo_xian@qq.com>
2022-09-02 22:06:01 +0800

    后台可以检测最新版本

--- admin/function/about.php
+++ admin/function/about.php
@@ -1,5 +1,6 @@
 <?php
     include "../../config.php";
+    include "./action/Get_latest.php";
 
     // 判断是否存在cookie且符合密码
     if (isset($_COOKIE['cookie']) && $_COOKIE['cookie'] === md5($admin_password)) {
@@ -49,7 +50,7 @@
         </ul>
         <span class="mdui-typo-title">学生电子成绩管理系统</span>
         <div class="mdui-toolbar-spacer"></div>
-        <a href="./logout.php" class="mdui-btn mdui-btn-icon" mdui-tooltip="{content: '退出'}"><i class="mdui-icon material-icons">exit_to_app</i></a>
+        <a href="./../logout.php" class="mdui-btn mdui-btn-icon" mdui-tooltip="{content: '退出'}"><i class="mdui-icon material-icons">exit_to_app</i></a>
     </div>
     <div class="mdui-typo" style="text-align: center;">
         <h2>关于我们</h2>
@@ -58,30 +59,56 @@
         <div class="mdui-typo">
             <hr/>
         </div>
-        <h4 style="margin: 30px auto">系统信息</h4>
+        <h4 class="mdui-center">系统信息</h4>
+        <div class="mdui-dialog" id="Update_content">
+            <div class="mdui-dialog-content">
+                <div class="mdui-dialog-title">更新日期（<?php echo $data['update_time']; ?>）</div>
+                <p class="mdui-text-left"><?php echo $data['update_content']; ?></p>
+            </div>
+            <div class="mdui-dialog-actions">
+                <button class="mdui-btn mdui-ripple mdui-text-color-black" mdui-dialog-close>确定</button>
+            </div>
+        </div>
+        <div class="mdui-p-b-3">
+            <button class="mdui-btn mdui-btn-raised mdui-ripple" οnclick=""><i class="mdui-icon material-icons">&#xe7fd;</i>作者网站</button>
+            <button class="mdui-btn mdui-btn-raised mdui-ripple"><i class="mdui-icon material-icons">&#xe838;</i>项目主页</button>
+            <button class="mdui-btn mdui-btn-raised mdui-ripple" mdui-dialog="{target: '#Update_content'}"><i class="mdui-icon material-icons">&#xe41a;</i>更新日志</button>
+        </div>
     </div>
-            <ul class="mdui-list mdui-shadow-3" style="margin-left: 680px !important;margin-right: 680px !important;">
+        <ul class="mdui-list mdui-shadow-3 mdui-m-x-5">
+            <li class="mdui-list-item mdui-ripple">
+                <i class="mdui-icon material-icons">&#xe322;</i>&nbsp;
+                学生电子成绩管理系统当前版本
+                <div class="mdui-list-item-content mdui-text-right">Ver 0.0.7 UTF-8</div>
+            </li>
+            <li class="mdui-list-item mdui-ripple">
+                <i class="mdui-icon material-icons">&#xe2bd;</i>&nbsp;
+                学生电子成绩管理系统最新版本
+                <div class="mdui-list-item-content mdui-text-right">Ver <?php echo $data['latest_version'] ?> UTF-8</div>
+            </li>
+            <li class="mdui-list-item mdui-ripple">
+            <i class="mdui-icon material-icons">&#xe875;</i>&nbsp;
+                服务器信息
+                <div class="mdui-list-item-content mdui-text-right"><?php echo $_SERVER['SERVER_SOFTWARE'] ?></div>
+            </li>
+            <li class="mdui-list-item mdui-ripple">
+                <i class="mdui-icon material-icons">&#xe80b;</i>&nbsp;
+                服务器 PHP 版本
+                <div class="mdui-list-item-content mdui-text-right"><?php echo substr(PHP_VERSION,0,6); ?></div>
+            </li>
+            <li class="mdui-list-item mdui-ripple">
+                <i class="mdui-icon material-icons">&#xe6c4;</i>&nbsp;
+                服务器 MYSQL 版本
+                <div class="mdui-list-item-content mdui-text-right"><?php $link = @mysqli_connect($host,$username,$password,$dbname,$port); printf(substr($link -> server_info,0,6)); ?></div>
+            </li>
+            <a class="mdui-ripple" href="https://ip.sanv.org/index.php?ip=<?php echo $_SERVER['REMOTE_ADDR']; ?>" target="_blank">
                 <li class="mdui-list-item mdui-ripple">
-                    <i class="mdui-list-item-icon mdui-icon material-icons">info</i>&nbsp;
-                    学生电子成绩管理系统程序版本
-                    <div class="mdui-list-item-content mdui-text-right">Ver 0.0.7 UTF-8</div>
+                    <i class="mdui-icon material-icons">&#xe250;</i>&nbsp;
+                    正在连接的 IP
+                    <div class="mdui-list-item-content mdui-text-right"><?php echo $_SERVER['REMOTE_ADDR']; ?></div>
                 </li>
-                <li class="mdui-list-item mdui-ripple">
-                    <i class="mdui-list-item-icon mdui-icon material-icons">cloud</i>&nbsp;
-                    学生电子成绩管理系统最新版本
-                    <div class="mdui-list-item-content mdui-text-right">Ver 9.9.9 UTF-8</div>
-                </li>
-                <li class="mdui-list-item mdui-ripple">
-                    <i class="mdui-list-item-icon mdui-icon material-icons">public</i>&nbsp;
-                    服务器 PHP 版本
-                    <div class="mdui-list-item-content mdui-text-right"><?php echo substr(PHP_VERSION,0,3); ?></div>
-                </li>
-                <li class="mdui-list-item mdui-ripple">
-                    <i class="mdui-list-item-icon mdui-icon material-icons">pie_chart</i>&nbsp;
-                    服务器 MYSQL 版本
-                    <div class="mdui-list-item-content mdui-text-right"><?php $link = @mysqli_connect($host,$username,$password,$dbname,$port); printf(substr($link -> server_info,0,3)); ?></div>
-                </li>
-            </ul>
+            </a>
+        </ul>
     <script src="https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/mdui/1.0.2/js/mdui.min.js" type="application/javascript"></script>
 </body>
 </html>
---
+++ admin/function/action/Get_latest.php
@@ -0,0 +1,46 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$header[] = "Content-type: text/json";
+$header[] = "user-agent: nuoixanCN / Beautiful-Report-Card";
+
+// 初始化Curl会话
+$curl = curl_init();
+
+// 请求的网址
+curl_setopt($curl, CURLOPT_URL, "https://api.github.com/repos/nuoxianCN/Beautiful-Report-Card/releases/latest");
+// 隐藏返回响应信息头
+curl_setopt($curl, CURLOPT_HEADER, false);
+// 请求标头
+curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+// 文件流返回
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+// 检测是否报错
+if ($error = curl_error($curl)) {
+    // 报错输出
+    echo "error: " .$error;
+} else {
+    // 正常输出
+    // header('Content-type: application/json');
+
+    $data = curl_exec($curl);
+    $data = json_decode($data, true);
+
+    $json = array(
+        'code' => 200,
+        'node_id' => $data['node_id'],
+        'latest_version' => $data['tag_name'],
+        'publish_page' => $data['html_url'],
+        'download' => $data['zipball_url'],
+        'update_time' => $data['published_at'],
+        'update_content' => $data['body']
+    );
+
+    $data = json_encode($json, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    $data = json_decode($data, true);
+    //var_dump($data);
+}
+
+// 关闭Curl资源，并且释放系统资源
+curl_close($curl);
+?>
